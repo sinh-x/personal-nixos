0dce9e8914478ec214bcd3c9574b9be8c7cea294
Sinh Nguyen <sinh@sinhn.com>
2024-07-17 21:06:38 +0700

    Static LAN IP for Elderwood

--- hosts/Elderwood/default.nix
+++ hosts/Elderwood/default.nix
@@ -94,6 +94,11 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.ena eble = false;
+  networking.defaultGateway = "192.168.1.1";
+  networking.interfaces.wlo1.ipv4.addresses = [{
+    address = "192.168.1.4";
+    prefixLength = 24;
+  }];
   networking.wireless = {
     environmentFile = "/home/sinh/.config/wireless.env";
     enable = true;
