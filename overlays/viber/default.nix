0706958cea4edc4bac54ca6caeed73d8eef1299f
sinh-x <sinh@sinhn.com>
2025-02-06 14:43:07 +0700

    Remove viber overlay

--- overlays/viber/default.nix
+++ overlays/viber/default.nix
@@ -1,12 +1,12 @@
-_: _final: prev: {
+_: _final: _prev: {
   # For example, to pull a package from unstable NixPkgs make sure you have the
   # input `unstable = "github:nixos/nixpkgs/nixos-unstable"` in your flake.
-  viber = prev.viber.overrideAttrs (_oldAttrs: {
-    version = "23.2.0.3";
-    src = prev.fetchurl {
-      url = "https://web.archive.org/web/20240824071651/https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb";
-      sha256 = "sha256-9WHiI2WlsgEhCPkrQoAunmF6lSb2n5RgQJ2+sdnSShM=";
-    };
-  });
+  # viber = prev.viber.overrideAttrs (_oldAttrs: {
+  #   version = "23.2.0.3";
+  #   src = prev.fetchurl {
+  #     url = "https://web.archive.org/web/20240824071651/https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb";
+  #     sha256 = "sha256-9WHiI2WlsgEhCPkrQoAunmF6lSb2n5RgQJ2+sdnSShM=";
+  #   };
+  # });
 
 }
