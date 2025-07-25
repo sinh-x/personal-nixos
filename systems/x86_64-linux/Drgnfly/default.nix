754f06ce26c7780396d7f763a0ff67eaa75ff858
Sinh Nguyen <sinh@sinhn.com>
2025-07-25 18:24:04 +0700

    Remove claude-desktop

--- systems/x86_64-linux/Drgnfly/default.nix
+++ systems/x86_64-linux/Drgnfly/default.nix
@@ -5,11 +5,6 @@
   pkgs,
   ...
 }:
-let
-  claude-desktop = inputs.claude-desktop.packages.x86_64-linux.default.overrideAttrs (old: {
-    nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.autoPatchelfHook ];
-  });
-in
 {
   imports = [
     ./hardware-configuration.nix
@@ -147,7 +142,6 @@ in
     inputs.zen-browser.packages."${system}".twilight # artifacts are downloaded from this repository to guarantee reproducibility
     inputs.zen-browser.packages."${system}".twilight-official # artifacts are downloaded from the official Zen repository
 
-    claude-desktop
   ];
 
   # Open ports in the firewall.
