f84068673a7017002e1c45d219a9e3eccc5e014e
Sinh Nguyen <sinh@sinhn.com>
2025-11-25 11:25:12 +0700

    Update wifi-network

--- systems/x86_64-linux/Emberroot/wifi-networks.nix
+++ systems/x86_64-linux/Emberroot/wifi-networks.nix
@@ -181,6 +181,42 @@
       "Mango Tree" = {
         pskRaw = "ext:qn_mangotree";
       };
+      "BaiDinhHotel-R125" = {
+        pskRaw = "ext:hn_baidinh_hotel";
+      };
+      "Bar Coffee" = {
+        pskRaw = "ext:hn_baidinh_hotel";
+      };
+      "LPH House 5G" = {
+        pskRaw = "ext:hn_lphhouse";
+      };
+      "TENEMENT ZONE 1 5G" = {
+        pskRaw = "ext:hn_tenement";
+      };
+      "Hoianese Old Town f4b" = {
+        pskRaw = "ext:dn_hoianese";
+      };
+      "IKIGAI" = {
+        pskRaw = "ext:dn_ikigai";
+      };
+      "TRA Coffee & Tea House 5GHz" = {
+        pskRaw = "ext:dn_trateahouse";
+      };
+      "Seahorse Han Market by Haviland" = {
+        pskRaw = "ext:dn_joycafe";
+      };
+      "The Interlude" = {
+        pskRaw = "ext:dn_interlude";
+      };
+      "LAROSE COFFEE" = {
+        pskRaw = "ext:qn_larose_1";
+      };
+      "Larose Coffee" = {
+        pskRaw = "ext:qn_larose";
+      };
+      "CENTRE HOTEL" = {
+        pskRaw = "ext:dn_centre";
+      };
       "Pax Ana Resort" = { };
       "Starbucks" = { };
       "WORKFLOW CAFE" = { };
