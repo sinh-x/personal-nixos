574febea31dda751caae76b08c4e6879fee7770e
Sinh Nguyen <sinh@sinhn.com>
2026-06-15 13:55:19 +0700

    feat(viber): phase 2 - activate overlay with libxshmfence symlink

--- overlays/viber/default.nix
+++ overlays/viber/default.nix
@@ -1,12 +1,8 @@
-_: _final: _prev: {
-  # For example, to pull a package from unstable NixPkgs make sure you have the
-  # input `unstable = "github:nixos/nixpkgs/nixos-unstable"` in your flake.
-  # viber = prev.viber.overrideAttrs (_oldAttrs: {
-  #   version = "23.2.0.3";
-  #   src = prev.fetchurl {
-  #     url = "https://web.archive.org/web/20240824071651/https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb";
-  #     sha256 = "sha256-9WHiI2WlsgEhCPkrQoAunmF6lSb2n5RgQJ2+sdnSShM=";
-  #   };
-  # });
-
+{ lib, ... }:
+_final: prev: {
+  viber = prev.viber.overrideAttrs (oldAttrs: {
+    installPhase = oldAttrs.installPhase + ''
+      ln -s "${lib.getLib prev.libxshmfence}/lib/libxshmfence.so.1" "$out/opt/viber/lib/libxshmfence.so.1"
+    '';
+  });
 }
