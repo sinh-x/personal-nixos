856daa7d2c6c9a86ea8c3a884332c29ba89264bc
Sinh Nguyen <72894652+sinh-x@users.noreply.github.com>
2026-08-24 07:50:53 +0700

    NX-040: Set Vivaldi as Drgnfly default browser (#72)
    * chore: update pap + opencode
    
    * feat(drgnfly): phase 1 localize browser MIME policy

--- home/sinh/Drgnfly.nix
+++ home/sinh/Drgnfly.nix
@@ -37,6 +37,23 @@
     ];
   };
 
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      # Browser - vivaldi
+      "x-scheme-handler/http" = "vivaldi.desktop";
+      "x-scheme-handler/https" = "vivaldi.desktop";
+      "x-scheme-handler/chrome" = "vivaldi.desktop";
+      "text/html" = "vivaldi.desktop";
+      "application/xhtml+xml" = "vivaldi.desktop";
+      "application/x-extension-htm" = "vivaldi.desktop";
+      "application/x-extension-html" = "vivaldi.desktop";
+      "application/x-extension-shtml" = "vivaldi.desktop";
+      "application/x-extension-xhtml" = "vivaldi.desktop";
+      "application/x-extension-xht" = "vivaldi.desktop";
+    };
+  };
+
   sinh-x = {
     apps = {
       sinh-x = {
